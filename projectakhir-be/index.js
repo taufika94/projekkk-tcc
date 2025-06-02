8cf1f679c8bd44bb97addaba52a1fe2bc5952200
taufika <taufikaa94@gmail.com>
2025-06-03 01:05:38 +0700

    docker file

--- projectakhir-be/index.js
+++ projectakhir-be/index.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.render("index"); // Pastikan file index.ejs ada di dalam folder views
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
