5c048d2e0388dddf13f5d4b28138abc6c267b54d
taufika <taufikaa94@gmail.com>
2025-06-03 00:55:02 +0700

    docker file

--- projectakhir-be/config/Database.js
+++ projectakhir-be/config/Database.js
@@ -8,7 +8,7 @@ const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 
 //menyambungkan ke database
-const db = new Sequelize('gudangsenjata','root','',{
+const db = new Sequelize('RECOVER_YOUR_DATA','root','',{
     host : '34.60.142.253',
     dialect : "mysql"
 });
